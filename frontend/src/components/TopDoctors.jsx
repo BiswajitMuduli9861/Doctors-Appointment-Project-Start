9f6303f8869607475382b3c229ab10a188425e43
Biswajit Muduli <bmuduli126@gmail.com>
2026-01-02 19:55:10 +0530

    02-1-2025 frontend Home Page continue

---
+++ frontend/src/components/TopDoctors.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {doctors} from '../assets/assets'
+const TopDoctors = () => {
+  return (
+    <div>
+      <h1>Top Doctors to  Book</h1>
+      <p>Simply browse through our extensive list of trusted doctors.</p>
+      <div>
+        {
+            doctors.slice
+        }
+      </div>
+    </div>
+  )
+}
+
+export default TopDoctors
---
+++ frontend/src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='text-amber-500 bg-amber-600'>
+      about
+    </div>
+  )
+}
+
+export default About
