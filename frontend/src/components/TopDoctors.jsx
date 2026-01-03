43419bc0da22692086f31071ffb4f60fba92dc1e
Biswajit Muduli <bmuduli126@gmail.com>
2026-01-03 15:09:50 +0530

    feat: finish homepage UI and layout

--- frontend/src/components/TopDoctors.jsx
+++ frontend/src/components/TopDoctors.jsx
@@ -1,15 +1,32 @@
-import React from 'react'
-import {doctors} from '../assets/assets'
+import React, { useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { AppContext } from '../context/AppContext';
+
 const TopDoctors = () => {
+  const navigate = useNavigate();
+  const {doctors} = useContext(AppContext)
   return (
-    <div>
-      <h1>Top Doctors to  Book</h1>
-      <p>Simply browse through our extensive list of trusted doctors.</p>
-      <div>
-        {
-            doctors.slice
-        }
+    <div className='flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10'>
+      <h1 className='text-3xl font-medium'>Top Doctors to  Book</h1>
+      <p className='sm:w-1/3 text-center text-sm'>Simply browse through our extensive list of trusted doctors.</p>
+    <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 pt-5 px-3 sm:px-0">
+  {doctors.slice(0,10).map((item, index) => (
+    <div onClick={()=>navigate(`/appointment/${item._id}`)} key={index} className="border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-3 transition-transform duration-500">
+      <img className="bg-blue-50 w-full h-32 object-cover" src={item.image} alt="images" />
+      <div className="p-4">
+        <div className="flex items-center gap-2 text-sm text-green-500 mb-2">
+          <span className="w-2 h-2 bg-green-500 rounded-full"></span>
+          <p>Available</p>
+        </div>
+        <p className="font-semibold">{item.name}</p>
+        <p className="text-gray-500 text-sm">{item.speciality}</p>
       </div>
+    </div>
+  ))}
+</div>
+
+
+      <button onClick={()=>{navigate('/doctors'), scrollTo(0,0)}} style={{backgroundColor:"rgba(0, 0, 255, 0.3)", color:"black"}} className='px-12 py-3 rounded-full mt-10'>more</button>
     </div>
   )
 }
